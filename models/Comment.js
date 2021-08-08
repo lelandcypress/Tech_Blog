34147bd591700e09c1eb176d4ab435700a67dbb6
Michael Souter <souterm1@gmail.com>
2021-08-07 23:04:09 -0400

    models built and defined

---
+++ models/Comment.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    article_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "article",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,1 +1,17 @@
+const User = require("./User");
+const Article = require("./Article");
+const Comment = require("./Comment");
 
+Comment.hasOne(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Article, {
+  foreignKey: "article_id",
+});
+
+Article.hasOne(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Article, Comment };
